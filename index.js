a75a3470941de48b5504d5bb151fb82f10787752
Elnathan Mburu <elnathanmburu@gmail.com>
2023-10-14 23:23:20 +0300

    complete sir

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+
+const input = document.getElementById('input');//direct adding 
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
+/*const input2 = document.getElementById('input');//creating a callback function so as it can be reused later
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input2.addEventListener('click', clickAlert);*/
